5615fbec1730a7b299ccce1edad60d6e3fc4a1c1
odps <odps31@gmail.com>
2025-02-05 16:18:15 +0100

    [fix]: Arreglado problema con logout del usuario (no borraba cookies/cache del navegador)

--- app/Client/src/app/services/auth.service.ts
+++ app/Client/src/app/services/auth.service.ts
@@ -53,10 +53,35 @@ export class AuthService {
   }
 
   logout() {
+    //Limpia la cache/cookies del navegador y elimina el token de sesion
     localStorage.removeItem('token');
+    this.removeCache();
+    this.removeCookies();
+
+    //Avisa al servidor para que se cierre la sesion
     this.http.post<any>(`${environment.apiUrl}/logout`, {
       headers: environment.headers,
     });
     this.router.navigate(['/login']);
+  }
+
+  //Itera sobre la cache del navegador y borra cada elemento.
+  private removeCache() {
+    if ('caches' in window) {
+      caches.keys().then((names) => {
+        names.forEach((name) => {
+          caches.delete(name);
+        });
+      });
+    }
+  }
+
+  //Recoge las cookies del navegador y les asigna una fecha de expiracion en el pasado para que se borren.
+  private removeCookies() {
+    const cookies = document.cookie.split(';');
+    for (let cookie of cookies) {
+      const name = cookie.split('=')[0].trim();
+      document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    }
   }
 }
