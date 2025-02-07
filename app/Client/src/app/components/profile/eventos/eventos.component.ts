62de75f09e09dcd4c2bb037c90263387279fab3b
Brandon <josuebrandon17@gmail.com>
2025-02-07 11:35:11 +0100

    xd

---
+++ app/Client/src/app/components/profile/eventos/eventos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  standalone: false,
+  
+  templateUrl: './eventos.component.html',
+  styleUrl: './eventos.component.css'
+})
+export class EventosComponent {
+
+}
